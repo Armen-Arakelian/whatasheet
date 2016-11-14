221b362f107223c919294d4cf8d6c4c967974203
Armen Arakelian <yozhyn1@gmail.com>
2016-11-14 15:39:47 +0200

    image

--- Cursach/requestHandler.js
+++ Cursach/requestHandler.js
@@ -1,21 +1,21 @@
 /**
- * Created by ASA on 13.11.2016.
+ * Created by ASA on 12.11.2016.
  */
 
-var exec = require("child_process").exec;
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
         '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
+        '<meta http-equiv="Content-Type" '+
+        'content="text/html; charset=UTF-8" />'+
         '</head>'+
         '<body>'+
         '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="10" cols="30"></textarea>'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
         '<input type="submit" value="Submit text" />'+
         '</form>'+
         '</body>'+
@@ -34,6 +34,21 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response, postData) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("test.jpg", "binary", function(error, file) {
+        if(error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
+}
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
