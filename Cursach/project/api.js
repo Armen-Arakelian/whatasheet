20f848d7e06a2c27129d0afdd7bedf5a12d3b5d5
Armen Arakelian <yozhyn1@gmail.com>
2016-11-25 00:23:35 +0200

    restfull api

---
+++ Cursach/project/api.js
@@ -0,0 +1,104 @@
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+
+var morgan       = require('morgan');
+var bodyParser   = require('body-parser');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+
+
+module.exports = function(app) {
+    app.use(morgan('dev'));
+    app.use(bodyParser());
+
+    app.set('view engine', 'ejs');
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    var User = require('./app/models/user');
+
+
+    var router = express.Router();
+
+
+    router.get('/', function (req, res) {
+        res.json({message: 'api, set the url'});
+    });
+
+
+    router.route('/users')
+
+        .post(function (req, res) {
+
+            var user = new User();
+            user.local.email = req.body.email;
+
+            user.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json({message: 'User created!'});
+            });
+
+
+        })
+
+        .get(function (req, res) {
+            User.find(function (err, users) {
+                if (err)
+                    res.send(err);
+
+                res.json(users);
+            });
+        });
+
+    router.route('/users/:user_id')
+
+        .get(function (req, res) {
+            User.findById(req.params.user_id, function (err, user) {
+                if (err)
+                    res.send(err);
+                res.json(user);
+            });
+        })
+
+        .put(function (req, res) {
+            User.findById(req.params.user_id, function (err, user) {
+
+                if (err)
+                    res.send(err);
+
+                user.local.email = req.body.email;
+                user.save(function (err) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({message: 'User updated!'});
+                });
+
+            });
+        })
+
+        .delete(function (req, res) {
+            User.remove({
+                _id: req.params.user_id
+            }, function (err, user) {
+                if (err)
+                    res.send(err);
+
+                res.json({message: 'Successfully deleted'});
+            });
+        });
+
+
+    app.use('/api', router);
+}
+
+
+
+
+
