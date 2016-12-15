b8e44294ff49a4fc5382b2fd0873cc4baf53fda2
Armen Arakelian <yozhyn1@gmail.com>
2016-12-15 02:12:19 +0200

    update

--- Cursach/project/app/models/idea.js
+++ Cursach/project/app/models/idea.js
@@ -4,7 +4,12 @@ var mongoose = require('mongoose');
 var ideaSchema = mongoose.Schema({
         title   : String,
         text     : String,
-        picture : { data: Buffer, contentType: String }
+
+        picture : { data: Buffer, contentType: String },
+
+        comments: [{
+                comment: String
+        }]
 });
 
 module.exports = mongoose.model('Idea', ideaSchema);
